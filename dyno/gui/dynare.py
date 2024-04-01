16ec65749464e763e87f248c51bc748340fe5f40
Pablo Winant <pablo.winant@gmail.com>
2024-04-01 23:28:24 +0200

    Added basic solara interface.

--- dyno/gui/dynare.py
+++ dyno/gui/dynare.py
@@ -1,4 +1,4 @@
-def dyno_gui(filename):
+def dyno_gui(filename, parchoice={}):
     
     import solara
     from solara.alias import rv
@@ -54,6 +54,12 @@ def dyno_gui(filename):
     
     split_screen = solara.reactive(False)
     
+    import copy
+    args_0 = copy.copy(parchoice)
+
+
+    parameters = {k: solara.reactive(v[0]) for (k,v) in args_0.items()}
+
     
     def import_model(txt):
     
@@ -103,8 +109,11 @@ def dyno_gui(filename):
                 method = "qz"
             else:
                 method = "ti"
+
+            parms = {k:v for k,v in parameters.items()}
+
     
-            dr.value = model.solve(method=method)
+            dr.value = model.solve(method=method, calibration=parms)
     
             t2 = time.time()
     
@@ -129,139 +138,20 @@ def dyno_gui(filename):
         # ok.value = ok_import.value & ok_ss.value & ok_bk.value
         
     
-    import copy
-    
     @solara.component
     def ModelEditor():
-                            
+                        
         with solara.Column():
             
             rv.Textarea(v_model=text.value, on_v_model=import_model, rows=20, style_="font-family: monospace")
-    
-    @solara.component
-    def SolutionViewer(dr_):
-    
-        from IPython.core.display import display_html
-    
-        dr = dr_.value
-    
-        solara.Markdown("__Eigenvalues__")
-    
-        evv = pd.DataFrame(
-            [np.abs(dr.evs)],
-            columns = [i+1 for i in range(len(dr.evs))],
-            index=["λ"]
-        )
 
 
-        solara.display(evv)
-        # html_evs = display_html(evv)
-    
-        # print(html_evs)
-        # solara.HTML(tag="table", unsafe_innerHTML=html_evs, style="font-family: monospace")
-    
-        # solara.Markdown("### Decision Rule")
-        solara.Markdown("__Decision Rule__")
-    
-        hh_y = dr.X
-        hh_e = dr.Y
-    
+    from .components import SolutionViewer, SimulViewer2
+    from .components import ParameterChooser
 
-        df = pd.DataFrame(
-            np.concatenate([hh_y, hh_e], axis=1),
-            columns=["{}[t-1]".format(e) for e in dr.symbols['endogenous']]+["{}[t]".format(e) for e in (dr.symbols['exogenous'])]
-        )
-    
-        df.index = ["{}[t]".format(e) for e in dr.symbols['endogenous']]
-    
-        solara.display(df)
-    
-
-        df_moments = pd.DataFrame(
-            hh_y,
-            columns=["{}[t]".format(e) for e in (dr.symbols['endogenous'])],
-            index=["{}[t]".format(e) for e in (dr.symbols['endogenous'])]
-        )
-
-        solara.Markdown("__Moments__")
-
-        solara.display(df_moments)
-
-
-    
-    
-    def one_plot(irfs_, c, selects):
-    
-        irfs = irfs_.value
-    
-        k0 = [*irfs.keys()][0]
-    
-        x_ = irfs[k0][c].index
-    
-    
-        # create scales
-        xs = bqplot.LinearScale(min=0,max=len(x_)+1)
-        # ys = bqplot.LinearScale()
-    
-        colors=["blue","red"]
-        # with iw.Card(outlinedd=True,_style="width: 350px; height: 250px"):
-        lines = [
-            bqplot.Lines(x=x_, y=irfs[k][c], scales={"x": xs, "y": bqplot.LinearScale()}, labels=[k], colors=colors[i])
-            for (i,k) in enumerate(irfs['shocks'].unique()) if k in selects.value
-        ]
-        # create axes objects
-        # xax = bqplot.Axis(scale=xs, grid_lines="solid", label='t')
-        # yax = bqplot.Axis(scale=ys, orientation="vertical", label=c, grid_lines="solid")
-    
-        # create the figure object (renders in the output cell)
-        return bqplot.Figure(marks=lines, legend=True, transition=True)
-    
-    
-    from reacton import ipyvuetify as iw
-    
-    
-    
-    @solara.component
-    def SimulViewer(irfs, sim_grid):
-        
-        cols = [str(e) for e in [*irfs.value.values()][0].columns[1:]]
-        n = len(cols)
-    
-        ind, set_ind = solara.use_state(cols[0])
-    
-    
-    
-        if not sim_grid.value:
-            with iw.Window(v_model=ind, on_v_model=set_ind, show_arrows=True):
-                for (i,c) in enumerate(cols):
-                    with iw.WindowItem(value=c):
-                        # with iw.Card():
-                            one_plot(irfs, c, selects)
-        else:
-            with solara.ColumnsResponsive():
-                for (i,c) in enumerate(cols):
-                        # with iw.Card():
-                            one_plot(irfs, c, selects)
-            
-    
-    
-    def SimulViewer2(irfs_, sim_grid):
-            
-        import plotly.express as px
-        
-        irfs = irfs_.value
-    
-        fig = px.line(irfs, x='t', y='value', color='shock', facet_col="variable", facet_col_wrap=2)
-        fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
-        fig.update_yaxes(title_text = "", matches=None)
-        fig.update_xaxes(title_text = "")
-    
-        solara.FigurePlotly(fig)
-    
-    
     @solara.component
     def Diagnostic():
-    
+
             with solara.Card("Diagnostic"):
                 if model_description.value is not None:
                     solara.Text(model_description.value)
@@ -270,12 +160,11 @@ def dyno_gui(filename):
                 else:
                     v = solution_time.value
                     solara.Success(label=f"All Good!")
-                                   
+                                    
                 solara.Checkbox(label="Import", value=ok_import)
                 solara.Checkbox(label="Steady-state", value=ok_ss)
                 solara.Checkbox(label="Blanchard-Kahn", value=ok_bk)
-    
-    
+
     @solara.component
     def Page():
         
@@ -285,6 +174,9 @@ def dyno_gui(filename):
             with solara.Column():
                 
                 Diagnostic()
+
+                with solara.Card("Parameters"):
+                    ParameterChooser(args_0, parameters, import_model, text)
     
     
                 # with solara.Card("Simul Options"):
@@ -310,7 +202,7 @@ def dyno_gui(filename):
     
                 with solara.lab.Tab("Simulation"):
                     with solara.Card(elevation=2):
-                        SimulViewer2(simul, sim_grid)
+                        SimulViewer2(simul, sim_grid, selects)
                     
                     # solara.DataFrame(simul.value)
     
